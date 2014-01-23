69a3be1246aaa1cac3423ceab08f9211b2a66572
crccheck <c@crccheck.com>
2014-01-22 18:03:23 -0600

    update docstring

--- kawarimi.py
+++ kawarimi.py
@@ -1,18 +1,57 @@
 """
-Load special templates based on the request.
+Steathfully replace Django templates based on the request.
+
+
+Intro
+~~~~~
+
+If you want to dynamically change the ``{% extends ... %}`` in your Django
+templates, you would normally have to use a variable ``{% extends
+template_to_extend %}``. That sucks because you have to get that variable into
+your context and now you can't grep for template names used. Django-Kawarimi
+lets you switch to a parallel set of templates based on the request.
+
 
 Usage
 ~~~~~
 
-Add 'kawarimi.RequestMiddleWare' to your settings.MIDDLEWARE_CLASSES.
+Add 'kawarimi.RequestMiddleWare' to your settings.MIDDLEWARE_CLASSES like::
+
+    MIDDLEWARE_CLASSES = [
+        'kawarimi.RequestMiddleWare',
+        ...,
+    ]
 
 Add 'kawarimi.Loader' to the top of your settings.TEMPLATE_LOADERS. Since this
 loader is just a fancy filesystem loader, you can remove
-`django.template.loaders.filesystem.Loader` if you want.
+`django.template.loaders.filesystem.Loader` if you want::
+
+    TEMPLATE_LOADERS = [
+        'kawarimi.Loader',
+        # 'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    ]
+
+Finally, add ``KAWARIMI_TEMPLATES`` to your settings like:
+
+    KAWARIMI_TEMPLATES = os.path.join(BASE_PATH, 'project/bizzaro_templates/')
 
 
-Based on work done in https://github.com/callowayproject/dynamicloader which
-was based on https://github.com/johnboxall/django-ab
+A Warning
+~~~~~~~~~
+
+This is super sneaky. After adding this to your projects, you will find a
+problem a few months later and have no idea what's going on. You will spend
+hours trying to figure out the problem. You will discover that you changed the
+template using this and curse me.
+
+
+Prior Art
+~~~~~~~~~
+
+Based on work done in https://github.com/callowayproject/dynamicloader which was
+based on https://github.com/johnboxall/django-ab. If you need more features, you
+should check those projects out.
 """
 from threading import local
 
@@ -25,9 +64,6 @@ _thread_locals = local()
 class RequestMiddleWare(object):
     """
     A middleware that lets external code peek the request.
-
-    WISHLIST common logic between this and `Loader` to change the content_type
-    to application/json ?
     """
     def process_request(self, request):
         _thread_locals.request = request
